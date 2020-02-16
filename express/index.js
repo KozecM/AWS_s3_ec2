7737192d50c8661a47b8219fd73b539c46f0de26
kozecm <kozecm@oregonstate.edu>
2020-02-15 16:21:13 -0800

    merged in express and edited vue

--- express/index.js
+++ express/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const AWS = require('aws-sdk');
 
 const app = express();
 const port = 3000;
 app.use(cors());
 
 app.get('/info', (req, res) => {
-  res.send("THis IS nothing get fuckerd");
+  
+  res.send("THis IS nothing get outahere");
 });
+app.listen(port, () => console.log('Hello from port {{port}}'))
